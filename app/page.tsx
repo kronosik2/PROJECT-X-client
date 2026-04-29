e9c8d736127ee7bbc8bde89ae43958af3395a75c
kronosik2 <kronosik2@gmail.com>
2026-04-29 23:12:42 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@ import OrderForm from '@/components/OrderForm';
 
 export default function Home() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>ПРОЕКТ X — Грузчики</h1>
-      <OrderForm />
+    <div>
+      <h1>🚛 ПРОЕКТ X</h1>
+      <h1 style={{ fontSize: '20px', marginTop: '-10px', marginBottom: '32px' }}>Грузчики за 10 минут</h1>
+      
+      <div className="container">
+        <div className="form-card">
+          <OrderForm />
+        </div>
+      </div>
     </div>
   );
 }
