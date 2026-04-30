3d465d21f46b8fe87e4ce23f4bad3881cb009d6c
kronosik2 <kronosik2@gmail.com>
2026-04-30 21:08:24 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
+      <head>
+        {/* Убираем лишние мета-запросы */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>{children}</body>
     </html>
   );
